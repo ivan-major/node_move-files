49154d9dfd3c05cf51b99a847dd160974676cc23
tarstarcas@gmail.com <ivan.major.888@gmail.com>
2026-04-03 15:46:55 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const moveFile = (source, destination) => {
     throw new Error('Source and destination paths are required.');
   }
 
-  if (!fs.existsSync(source)) {
+  if (!fs.existsSync(source) || !fs.statSync(source).isDirectory()) {
     throw new Error(`Source file "${source}" does not exist.`);
   }
 
